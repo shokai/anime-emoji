013eb6a09e8e67be18727d6810f42bb3b743ea70
Sho Hashimoto <hashimoto@shokai.org>
2016-05-22 18:20:56 +0900

    get width

--- src/client/component/line.jsx
+++ src/client/component/line.jsx
@@ -4,14 +4,23 @@ export default class Line extends Component {
 
   static get propTypes(){
     return {
-      value: PropTypes.string.isRequired
+      value: PropTypes.string.isRequired,
+      onUpdate: PropTypes.func.isRequired
     }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    this.props.onUpdate()
+  }
+
+  componentDidMount(){
+    this.props.onUpdate()
   }
 
   render(){
     const {value} = this.props
     return (
-      <div className='line'>{value}</div>
+      <div className='line' >{value}</div>
     )
   }
 }
--- src/client/component/text-output.jsx
+++ src/client/component/text-output.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
+import {findDOMNode} from 'react-dom'
 import StoreComponent from './store-component'
 import Line from './line'
+import $ from 'jquery'
 
 export default class TextOutput extends StoreComponent {
 
-  componentDidUpdate(){
-    this.debug('didRender')
+  constructor(){
+    super()
+    this.onUpdateLine = this.onUpdateLine.bind(this)
   }
 
   render(){
     const {lines, size} = this.store.getState()
     let style = {
-      width: size + 2,
-      height: size + 2,
+      minWidth: size + 2,
+      minHeight: size + 2,
       fontSize: size
     }
     let _lines = []
     for(let i = 0; i < lines.length; i++){
-      _lines.push(<Line value={lines[i]} key={i} />)
+      _lines.push(<Line
+                  value={lines[i]}
+                  key={i}
+                  ref={`line:${i}`}
+                  onUpdate={() => this.onUpdateLine(i)}
+                  fontSize='1em'
+                  />)
     }
     return (
-      <div className='text-output' style={style}>{lines}</div>
+      <div className='text-output' style={style}>{_lines}</div>
     )
+  }
+
+  onUpdateLine(index){
+    this.debug(`onUpdateLine(${index})`)
+    setTimeout(() => {
+      const line = findDOMNode(this.refs[`line:${index}`])
+      if(!line) throw new Error(`cannot get DOM "line${index}"`)
+      console.log($(line).outerWidth(true))
+    }, 1)
   }
 
 }
