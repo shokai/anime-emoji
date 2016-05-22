927e1fde2ca94966c541ebc34d9192bf6c8348ec
Sho Hashimoto <hashimoto@shokai.org>
2016-05-22 12:20:08 +0900

    split lines

--- src/client/component/text-output.jsx
+++ src/client/component/text-output.jsx
@@ -5,8 +5,9 @@ export default class TextOutput extends StoreComponent {
 
   render(){
     const {text} = this.store.getState()
+    const lines = text.split(/[\r\n]/).map(line => <div>{line}</div>)
     return (
-      <div className='text-output'>{text}</div>
+      <div className='text-output'>{lines}</div>
     )
   }
 
